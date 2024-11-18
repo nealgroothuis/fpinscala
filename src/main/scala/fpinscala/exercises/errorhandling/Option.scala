b59fed58a9666a5b3325bab03f8879b0d9bf7d1c
Neal Groothuis <ngroothuis@slack-corp.com>
2024-11-18 06:10:53 -0500

    Exercise 4.1 Implement basic methods for Option

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -7,15 +7,25 @@ enum Option[+A]:
   case Some(get: A)
   case None
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match
+    case None    => None
+    case Some(a) => Some(f(a))
 
-  def getOrElse[B >: A](default: => B): B = ???
+  def getOrElse[B >: A](default: => B): B = this match
+    case None    => default
+    case Some(a) => a
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = this match
+    case None    => None
+    case Some(a) => f(a)
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match
+    case None    => ob
+    case Some(a) => Some(a)
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = this match
+    case Some(a) if f(a) => Some(a)
+    case _               => None
 
 object Option:
 
