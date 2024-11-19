d63ba60d35188ac8a750a7797732b17bfc83ec96
Neal Groothuis <ngroothuis@slack-corp.com>
2024-11-18 21:18:53 -0500

    Exercise 4.3: Implement map2()

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -56,7 +56,10 @@ object Option:
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match
+      case (Some(a), Some(b)) => Some(f(a, b))
+      case _                  => None
 
   def sequence[A](as: List[Option[A]]): Option[List[A]] = ???
 
