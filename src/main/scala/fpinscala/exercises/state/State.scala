997f9a02a8125fee703a94037fe14ee5037721e9
Neal Groothuis <ngroothuis@slack-corp.com>
2024-11-26 20:23:29 -0500

    Exercise 6.10: Implement basic State methods

--- src/main/scala/fpinscala/exercises/state/State.scala
+++ src/main/scala/fpinscala/exercises/state/State.scala
@@ -94,14 +94,20 @@ object State:
   extension [S, A](underlying: State[S, A])
     def run(s: S): (A, S) = underlying(s)
 
-    def map[B](f: A => B): State[S, B] =
-      ???
+    def map[B](f: A => B): State[S, B] = s =>
+      val (a, s1) = underlying(s)
+      (f(a), s1)
 
-    def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-      ???
+    def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = s =>
+      val (a, s1) = underlying(s)
+      val (b, s2) = sb(s1)
+      (f(a, b), s2)
 
-    def flatMap[B](f: A => State[S, B]): State[S, B] =
-      ???
+    def flatMap[B](f: A => State[S, B]): State[S, B] = s =>
+      val (a, s1) = underlying(s)
+      f(a)(s1)
+
+  def unit[S, A](a: A): State[S, A] = s => (a, s)
 
   def apply[S, A](f: S => (A, S)): State[S, A] = f
 
