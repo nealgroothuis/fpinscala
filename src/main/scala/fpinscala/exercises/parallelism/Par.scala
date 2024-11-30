8698075184ecac3a80f78261a52c42faebe56220
Neal Groothuis <ngroothuis@slack-corp.com>
2024-11-30 17:25:19 -0500

    Exercise 7.2: Representation of Par

--- src/main/scala/fpinscala/exercises/parallelism/Par.scala
+++ src/main/scala/fpinscala/exercises/parallelism/Par.scala
@@ -1,6 +1,8 @@
 package fpinscala.exercises.parallelism
 
-sealed trait Par[A]
+import java.util.concurrent.*
+
+opaque type Par[A] = ExecutorService => Future[A]
 object Par:
   extension [A](pa: Par[A])
      def map2[B, C](pb: Par[B])(f: (A, B) => C): Par[C] = ???
