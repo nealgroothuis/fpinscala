636cd5be5527ebcbc5208d365d78adf66f3c23ae
Neal Groothuis <ngroothuis@slack-corp.com>
2024-12-01 11:06:21 -0500

    Exercise 7.4: Implement asyncF()

--- src/main/scala/fpinscala/exercises/parallelism/Par.scala
+++ src/main/scala/fpinscala/exercises/parallelism/Par.scala
@@ -37,3 +37,5 @@ object Par:
     es => es.submit(new Callable[A] { def call = a(es).get })
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
